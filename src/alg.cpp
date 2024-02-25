e4c5048da258c6b71ff5470147004795b7459c18
laal28 <129623228+laal28@users.noreply.github.com>
2024-02-25 19:16:39 +0300

    Everything solved

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,18 +3,38 @@
 #include "alg.h"
 
 
-bool checkPrime(uint64_t value) {
-// вставьте код функции
+bool checkPrime(uint64_t value)
+{
+    if (value <= 1)
+        return false;
+
+    for ( uint64_t k = 2; k <= value / 2; k++ )
+        if (value % k == 0)
+            return false;
+
+    return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t nPrime(uint64_t n)
+{
+    uint64_t number = 0;
+    for( uint64_t i = 0; i < n; number = nextPrime( number ), ++i );  
+
+    return number;
 }
 
-uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+uint64_t nextPrime(uint64_t value)
+{
+    for( uint64_t i = value + 1; ; ++i )
+        if( checkPrime( i ) )
+            return i;  
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sumPrime(uint64_t hbound)
+{
+    uint64_t sum = 0;
+    for( uint64_t i = nextPrime( 0 ); i < hbound; i = nextPrime( i ) )
+        sum += i;
+
+    return sum;
 }
